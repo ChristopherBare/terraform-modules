84420b289f49b5a76b2a24b0d2b157a4b6c13067
Christopher Bare <christopherbare@outlook.com>
2023-11-21 11:22:48 -0500

    bucket

--- s3_website/main.tf
+++ s3_website/main.tf
@@ -15,7 +15,7 @@ resource "aws_cloudfront_distribution" "website_distribution" {
   default_root_object = var.index_document
 
   origin {
-    domain_name = aws_s3_bucket_website_configuration.website_config.website_domain
+    domain_name = aws_s3_bucket_website_configuration.website_config.bucket
     origin_id   = var.origin_id
 
     s3_origin_config {
