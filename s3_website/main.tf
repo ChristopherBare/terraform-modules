8c96e7343288506f0048822427452f2aaeeaf746
Christopher Bare <christopherbare@outlook.com>
2023-11-21 14:41:00 -0500

    Trying something

--- s3_website/main.tf
+++ s3_website/main.tf
@@ -55,18 +55,21 @@ resource "aws_cloudfront_origin_access_identity" "website_access_identity" {
 
 data "aws_iam_policy_document" "s3_policy" {
   statement {
-    sid       = "AllowPublic"
-    effect    = "Allow"
+    sid    = "AllowPublic"
+    effect = "Allow"
     principals {
       type        = "AWS"
       identifiers = ["*"]
     }
-    actions   = ["s3:GetObject"]
-    resources = ["${aws_s3_bucket.website_bucket.arn}/*"]
+    actions = ["s3:GetObject"]
+    resources = [
+      aws_s3_bucket.website_bucket.arn,
+      "${aws_s3_bucket.website_bucket.arn}/*"
+    ]
   }
 }
 
-resource "aws_s3_bucket_policy" "bucket_policy"{
+resource "aws_s3_bucket_policy" "bucket_policy" {
   bucket = aws_s3_bucket.website_bucket.id
   policy = data.aws_iam_policy_document.s3_policy.json
 }
