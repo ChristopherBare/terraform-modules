08fa0201a327764cc69ef7eea7d874aca8379f58
Christopher Bare <christopherbare@outlook.com>
2023-11-21 13:34:45 -0500

    Cloudfront policy

--- s3_website/main.tf
+++ s3_website/main.tf
@@ -57,12 +57,6 @@ data "aws_iam_policy_document" "bucket_policy" {
   statement {
     actions   = ["s3:GetObject"]
     resources = [aws_s3_bucket.website_bucket.arn]
-
-    principals {
-      type        = "AWS"
-      identifiers = [aws_cloudfront_origin_access_identity.website_access_identity.iam_arn]
-    }
-
     condition {
       test     = "StringLike"
       variable = "aws:UserAgent"
