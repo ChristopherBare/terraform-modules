cc018e848d7349820f5ee57489d8518a5163b265
Christopher Bare <christopherbare@outlook.com>
2023-11-21 14:35:56 -0500

    Trying something

--- s3_website/main.tf
+++ s3_website/main.tf
@@ -68,6 +68,6 @@ data "aws_iam_policy_document" "s3_policy" {
 
 resource "aws_s3_bucket_policy" "bucket_policy"{
   bucket = aws_s3_bucket.website_bucket.id
-  policy = data.aws_iam_policy_document.s3_policy
+  policy = data.aws_iam_policy_document.s3_policy.json
 }
 
