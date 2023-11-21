e90059b2d0906f21ee2eec39c58025e03a266724
Christopher Bare <christopherbare@outlook.com>
2023-11-21 12:03:59 -0500

    bucket

--- s3_website/outputs.tf
+++ s3_website/outputs.tf
@@ -4,3 +4,7 @@ output "website_url" {
   description = "URL for accessing the website"
   value       = aws_cloudfront_distribution.website_distribution.domain_name
 }
+
+output "bucket_name" {
+  value = aws_s3_bucket.website_bucket.bucket
+}
